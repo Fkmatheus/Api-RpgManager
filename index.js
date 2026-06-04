b8dd6c503fe5439b8e0b6b9caa67641cf02df4f9
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2026-06-04 16:15:33 -0300

    feat: Liberando cors para o front-end consumir

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 
@@ -8,6 +9,7 @@ const routes = require("./routes/routes");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 connection
     .authenticate()
